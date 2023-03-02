71d90c739706b7e05e9b7138f79997c62639c166
lyoung <lashellyoung15@gmail.com>
2023-03-01 21:48:03 -0600

    created springboot project

---
+++ src/main/java/com/young/autumn/autumn_selections/AutumnSelectionsApplication.java
@@ -0,0 +1,13 @@
+package com.young.autumn.autumn_selections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnSelectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutumnSelectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/young/autumn/autumn_selections/AutumnSelectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.autumn.autumn_selections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutumnSelectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
