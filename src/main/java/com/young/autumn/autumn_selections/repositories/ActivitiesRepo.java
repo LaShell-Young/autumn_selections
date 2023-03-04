6cb9a9102334a10e139caa5a5cd16fbed93e978d
lyoung <lashellyoung15@gmail.com>
2023-03-03 18:27:18 -0600

    wip

--- src/main/java/com/young/autumn/autumn_selections/repositories/ActivitiesRepo.java
+++ src/main/java/com/young/autumn/autumn_selections/repositories/ActivitiesRepo.java
@@ -1,12 +1,13 @@
 package com.young.autumn.autumn_selections.repositories;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import com.young.autumn.autumn_selections.models.Activity;
 
 import org.bson.types.ObjectId;
 
 @Repository
-public interface ActivitiesRepo extends MongoRepository<Activity, ObjectId>{
+public interface ActivitiesRepo extends MongoRepository<Activity, ObjectId> {
     
 }
